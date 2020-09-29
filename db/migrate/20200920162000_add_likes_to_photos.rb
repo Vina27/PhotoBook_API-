fb77f761946c9b7e88618ddfcd4e9d43e97e7e5b
Vina27 <Mtabali27@gmail.com>
2020-09-29 14:57:11 -0400

    Added likes model

---
+++ db/migrate/20200920162000_add_likes_to_photos.rb
@@ -0,0 +1,6 @@
+class AddLikesToPhotos < ActiveRecord::Migration[6.0]
+    def change
+     add_column :photos, :likes, :integer
+    end
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_161940) do
+ActiveRecord::Schema.define(version: 2020_09_20_162000) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_161940) do
     t.string "img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes"
   end
 
   create_table "users", force: :cascade do |t|
