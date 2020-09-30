ecb90e4fe6054d499ed33976f2d6b40e2e55f7f8
Vina27 <Mtabali27@gmail.com>
2020-09-30 16:29:33 -0400

    Fixed Likes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,11 +5,8 @@ class Photo < ApplicationRecord
   after_initialize :init
 
   def init
-    `self.likes = 0 if self.likes.nil?
-    # if self.likes == null 
-    #   self.likes = 0 
-    # end 
-  end
-
+    self.likes = 0 if self.likes.nil?
+     
+  end 
 
 end
