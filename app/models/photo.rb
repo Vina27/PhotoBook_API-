ef686f5ca4a075000e6940e5f1cb87c223662993
Vina27 <Mtabali27@gmail.com>
2020-09-30 15:27:54 -0400

    Like button done and updated README

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,15 @@
 class Photo < ApplicationRecord
   belongs_to :book 
   has_many :comments
+
+  after_initialize :init
+
+  def init
+    `self.likes = 0 if self.likes.nil?
+    # if self.likes == null 
+    #   self.likes = 0 
+    # end 
+  end
+
+
 end
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,4 +1,4 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :description, :img_url
+  attributes :id, :description, :img_url, :likes
   has_one :book_id
 end
